e96ab9fea2a3c756b1f96aeda8cc3e066ac90349
victortoru <victortobaruelaruiz@elpuig.xeill.net>
2023-02-15 19:52:25 +0100

    nuevo

--- src/main/java/ex1/Enunciat.java
+++ src/main/java/ex1/Enunciat.java
@@ -3,6 +3,7 @@ package ex1;
 public class Enunciat {
 /*
     1. Fes les modificacions necessàries perquè aquest projecte funcioni correctament fent servir Maven.
+        He afegit un Framework amb maven a la carpeta del projecte i he fet les modificacions necessàries i els test.
     2. Puja el projecte a GitHub i activa les accions necessàries perquè, en fer commit, s'executin els jocs de proves
        que muntaràs en els següents exercicis. Pots executar qualsevol joc de proves del package que vulguis (o més
        d'un) però és important demostrar que, efectivament, s'estan executant les proves mostrant el log de GitHub.
